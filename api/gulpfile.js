97c842451f57c7dd1e3b7ca98791ff15164280c6
James <choo1991@uw.edu>
2016-05-08 22:41:10 -0700

    Set up css. Grub watch task for css file and angular controller code change not working

--- api/gulpfile.js
+++ api/gulpfile.js
@@ -1,15 +1,6 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var ngAnnotate = require('gulp-ng-annotate');
+var fs = require('fs');
 
-/*
-load js files into gulp with stream then output to gulp.dest
-*/
-gulp.task('js', function () {
-	gulp.src(['ng/module.js', 'ng/**/*.js'])
-		.pipe(concat('app.js'))
-		.pipe(ngAnnotate())
-		.pipe(uglify())
-		.pipe(gulp.dest('assets'));
+fs.readdirSync(__dirname + '/gulp').forEach(function (task) {
+	require('./gulp/' + task);
 });
+
